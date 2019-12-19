bf491651bb4891d57e736a4cc1b772baa9debb13
Owline <biuro.owline@gmail.com>
2019-12-19 19:29:06 +0100

    header fixed position

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,22 @@
 if (module.hot) {
   module.hot.accept();
 }
+
+jQuery('.js-open-faq').click(function(){
+  if (jQuery(this).parent(".dropdown__top").hasClass("open")) {
+
+    jQuery(this).parent(".dropdown__top").removeClass("open");
+    jQuery(this).parent('.dropdown__top').siblings(".dropdown__btm").slideUp(500);
+
+  } else {
+
+    jQuery(this).parent(".dropdown__top").addClass("open");
+    jQuery(this).parent('.dropdown__top').siblings(".dropdown__btm").slideDown(500);
+
+    // jQuery(".dropdown").removeClass("open");
+    // jQuery(".dropdown .dropdown__btm").slideUp(500);
+    // jQuery(this).parent(".dropdown").addClass("open");
+    // jQuery(this).siblings(".dropdown__btm").slideDown(500);
+
+  }
+})
