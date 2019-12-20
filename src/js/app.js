3fef41e58b31f40cfdd314d76ef50f76ec2e9fef
Owline <biuro.owline@gmail.com>
2019-12-20 01:20:06 +0100

    add content

--- src/js/app.js
+++ src/js/app.js
@@ -4,19 +4,28 @@ if (module.hot) {
 
 jQuery('.js-open-faq').click(function(){
   if (jQuery(this).parent(".dropdown__top").hasClass("open")) {
-
     jQuery(this).parent(".dropdown__top").removeClass("open");
     jQuery(this).parent('.dropdown__top').siblings(".dropdown__btm").slideUp(500);
-
   } else {
-
     jQuery(this).parent(".dropdown__top").addClass("open");
     jQuery(this).parent('.dropdown__top').siblings(".dropdown__btm").slideDown(500);
-
-    // jQuery(".dropdown").removeClass("open");
-    // jQuery(".dropdown .dropdown__btm").slideUp(500);
-    // jQuery(this).parent(".dropdown").addClass("open");
-    // jQuery(this).siblings(".dropdown__btm").slideDown(500);
-
   }
 })
+
+// sticky header
+
+function fixedMenuPadding() {
+  let pt = document.querySelector('.js-site-header');
+  if(!pt) {
+    return;
+  }
+  let pto = pt.getBoundingClientRect();
+  if(! pt.classList.contains('js-no-padding')){
+    document.body.style.paddingTop = pt.height + 'px';
+  }
+}
+
+fixedMenuPadding();
+
+
+
