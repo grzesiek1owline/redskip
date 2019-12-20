d124b18ed34b2b15c7de499db06fe7cadf1d5339
Owline <biuro.owline@gmail.com>
2019-12-20 21:21:27 +0100

    mobile menu 1.0

--- src/js/app.js
+++ src/js/app.js
@@ -53,5 +53,26 @@ function fixedMenuPadding() {
 
 fixedMenuPadding();
 
+function toggleMenu(event,button,nav) {
+  event.preventDefault();
+  button.classList.toggle('open');
+  nav.classList.toggle('active');
+}
+
+function hamburger() {
+  let hamburger = document.querySelector('.js-toggle-menu');
+  let menu = document.querySelector('.js-mobile-menu');
+
+  console.log(hamburger);
+  console.log(menu);
+
+  if(! hamburger || ! menu) {
+    return;
+  }
+  console.log('ok');
+  hamburger.addEventListener('click', (e) => toggleMenu(e, hambuger, menu));
+}
+
+hamburger();
 
 
